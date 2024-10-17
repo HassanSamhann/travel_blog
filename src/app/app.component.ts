1fec98a3637741bac88eaef2091327f9588aca14
hassan <hassan700019@gmail.com>
2024-10-17 14:36:56 +0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../components/header/header.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MainComponent } from "../components/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule, MainComponent],
   styleUrl: './app.component.css', 
   template : `
 
   <app-header></app-header>  
+  <app-main></app-main>
   `
 })
 export class AppComponent {
--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -28,37 +28,7 @@ import { Component ,HostListener } from '@angular/core';
             <i class="fa-solid fa-bars bars"  (click)="display_nav()"></i>
         </nav>
     </header>
-    <div class="main-d">
-    <div class="nav-cola" id="colaps">
-    <i class="fa-solid fa-circle-xmark exit" (click)=" hide_nav()"></i>
-        <ul class="cola">
-                <li><a href="">Home</a></li>
-                <li><a href="">Destinations</a></li>
-                <li><a href="">Blogs</a></li>
-                <li><a href="">Reviews</a></li>
-                <li><a href="">Travel Tips</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-
-            </ul>
-            <div class="sgins">
-                <a class="sins" href="">Sign Up</a>
-                <a class="logs" href="">Login</a>
-            </div>
-
-        </div>
-
-        <div class="main-text">
-            "Unlock the World: Your Guide to Travel Blogs"
-        </div>
-        <div class="main-ipu">
-            <input type="text" placeholder="What would you like to review ?">
-            <i class="fa-solid fa-magnifying-glass  search"></i>
-        </div>
-        <div class="main-bot">
-            <img src="./images/h1-rev-bottom 2.png" alt="">
-        </div>
-    </div>
+    
   `
   
 })
@@ -78,12 +48,6 @@ export class HeaderComponent {
       let cola = <HTMLElement>  document.getElementById("colaps"); 
       cola.style.width = "100%" ; 
         exit.style.display = "block"
-    }
-    hide_nav(){
-        let exit = <HTMLElement> document.querySelector(".exit") ; 
-      let cola = <HTMLElement>  document.getElementById("colaps"); 
-      cola.style.width = "0%" ; 
-        exit.style.display = "none"
     }
     
     
