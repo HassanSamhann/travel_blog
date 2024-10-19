c00638f51c7b700d04263038fc6e4df211e30b6a
hassan <hassan700019@gmail.com>
2024-10-19 14:07:04 +0300

    commet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../components/header/header.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MainComponent } from "../components/main/main.component";
+import { AboutComponent } from "../components/about/about.component";
+import { ActivitiesComponent } from "../components/activities/activities.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule, MainComponent, AboutComponent, ActivitiesComponent],
   styleUrl: './app.component.css', 
   template : `
 
   <app-header></app-header>  
   <app-main></app-main>
+  <app-about></app-about>
+  <app-activities></app-activities>
   `
 })
 export class AppComponent {
---
+++ src/components/activities/activities.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activities',
+  standalone: true,
+  imports: [],
+  styleUrl: './activities.component.css',
+  template: `
+  <header class="container m-auto d-flex justify-content-between ">
+    <div>
+      <h1 class="metal-mania-regular h1Title"><span>What</span> Journey Will Inspire You Today?</h1>
+    <p class="Urbanist-font">Making the Most of Your Travel Experience in 2023</p>
+    </div>
+  </header>
+  <div class="activites">
+    <div class="act-title">
+      <h2>ACTIVITIES</h2>
+    </div>
+    
+  </div>
+  
+  
+  `
+
+
+})
+export class ActivitiesComponent {
+
+}
