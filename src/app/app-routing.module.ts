489aa215736c6eec534b32187ff7b7f770b4c0f2
hassan <hassan700019@gmail.com>
2024-10-20 21:05:20 +0300

    q

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // استيراد المكونات التي تريد توجيهها
-import { HomeComponent } from '../components/pages/home';
-import { DestinationsComponent } from './destinations/destinations.component';
-import { BlogsComponent } from './blogs/blogs.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { TravelTipsComponent } from './travel-tips/travel-tips.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from '../components/pages/home/home.component';
+import { DestinationsComponent } from '../components/pages/destinations/destinations.component';
+import { BlogsComponent } from '../components/pages/blogs/blogs.component';
+import { ReviewsComponent } from '../components/pages/reviews/reviews.component';
+import { TravelTipsComponent } from '../components/pages/travel-tips/travel-tips.component';
+import { AboutComponent } from '../components/pages/about/about.component';
+import { ContactComponent } from '../components/pages/contact/contact.component';
+import { SignUpComponent } from '../components/pages/sign-up/sign-up.component';
+import { LoginComponent } from '../components/pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
