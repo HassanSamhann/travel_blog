b860ea07c7bcf810decfbc5072372d1d2a3dd567
hassan <hassan700019@gmail.com>
2024-10-21 14:44:58 +0300

    review

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { AboutComponent } from "../../about/about.component";
 import { BlogsComponent } from "../blogs/blogs.component";
 import { ExploreComponent } from "../../explore/explore.component";
 import { GuideComponent } from "../../guide/guide.component";
+import { ReviewComponent } from "../../review/review.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainComponent, AboutComponent, BlogsComponent, ExploreComponent, GuideComponent],
+  imports: [MainComponent, AboutComponent, BlogsComponent, ExploreComponent, GuideComponent, ReviewComponent],
   styleUrl: './home.component.css', 
   template:`
   
@@ -16,6 +17,7 @@ import { GuideComponent } from "../../guide/guide.component";
   <app-about></app-about>
   <app-explore></app-explore>
   <app-guide></app-guide>
+  <app-review></app-review>
   `
 })
 export class HomeComponent {
