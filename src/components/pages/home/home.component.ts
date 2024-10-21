8d92c2d34684c1d4b071d0312a4335f259f10674
hassan <hassan700019@gmail.com>
2024-10-21 10:50:34 +0300

    commits

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { MainComponent } from "../../main/main.component";
+import { AboutComponent } from "../../about/about.component";
+import { BlogsComponent } from "../blogs/blogs.component";
+import { ExploreComponent } from "../../explore/explore.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainComponent],
+  imports: [MainComponent, AboutComponent, BlogsComponent, ExploreComponent],
   styleUrl: './home.component.css', 
   template:`
   
   <app-main></app-main>
-  
+  <app-about></app-about>
+  <app-explore></app-explore>
   `
 })
 export class HomeComponent {
