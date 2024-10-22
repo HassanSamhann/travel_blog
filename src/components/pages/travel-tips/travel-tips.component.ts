63ec8031a2cb5fcdf41159d84821fe05d7f70e2d
hassan <hassan700019@gmail.com>
2024-10-23 01:23:57 +0300

    gf

--- src/components/pages/travel-tips/travel-tips.component.ts
+++ src/components/pages/travel-tips/travel-tips.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { MainComponent } from "../../main/main.component";
+import { TopComponent } from "../../top/top.component";
 
 @Component({
   selector: 'app-travel-tips',
   standalone: true,
-  imports: [MainComponent],
+  imports: [MainComponent, TopComponent],
   styleUrl: './travel-tips.component.css', 
 
   template:`
   <app-main image="./trip.jpg"  text="Our Journey: Inspiring Travel, One Trip at a Time"></app-main>
-
+  <app-top></app-top>
   `
 
 
