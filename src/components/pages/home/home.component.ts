02e94bd36d1118a2b925a252ad8cd47ae2304809
hassan <hassan700019@gmail.com>
2024-10-22 15:14:04 +0300

    qw

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -3,21 +3,20 @@ import { MainComponent } from "../../main/main.component";
 import { AboutComponent } from "../../about/about.component";
 import { BlogsComponent } from "../blogs/blogs.component";
 import { ExploreComponent } from "../../explore/explore.component";
-import { GuideComponent } from "../../guide/guide.component";
 import { ReviewComponent } from "../../review/review.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainComponent, AboutComponent, BlogsComponent, ExploreComponent, GuideComponent, ReviewComponent],
+  imports: [MainComponent, AboutComponent, BlogsComponent, ExploreComponent, ReviewComponent],
   styleUrl: './home.component.css', 
   template:`
   
   <app-main></app-main>
   <app-about></app-about>
   <app-explore></app-explore>
-  <app-guide></app-guide>
   <app-review></app-review>
+  
   `
 })
 export class HomeComponent {
