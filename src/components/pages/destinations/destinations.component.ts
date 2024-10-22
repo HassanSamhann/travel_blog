fd2508d909d718acb69194ee59d98bcd8cb07181
hassan <hassan700019@gmail.com>
2024-10-22 18:28:08 +0300

    des

--- src/components/pages/destinations/destinations.component.ts
+++ src/components/pages/destinations/destinations.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MainComponent } from "../../main/main.component";
+import { ActivitiesComponent } from "../../activities/activities.component";
 
 @Component({
   selector: 'app-destinations',
   standalone: true,
-  imports: [],
-  templateUrl: './destinations.component.html',
-  styleUrl: './destinations.component.css'
+  imports: [MainComponent, ActivitiesComponent],
+  styleUrl: './destinations.component.css',
+  template:`
+  
+  <app-main></app-main>
+    <app-activities></app-activities>
+  
+  `
 })
 export class DestinationsComponent {
 
