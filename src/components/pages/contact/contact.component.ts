d975f3478740063ddd16de7f61902b7801890af9
hassan <hassan700019@gmail.com>
2024-10-23 14:45:45 +0300

    contact

--- src/components/pages/contact/contact.component.ts
+++ src/components/pages/contact/contact.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { MainComponent } from "../../main/main.component";
+import { ContaComponent } from "../../conta/conta.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MainComponent],
+  imports: [MainComponent, ContaComponent],
   styleUrl: './contact.component.css',
 
   template:`
   
   <app-main image="./contact.jpg"  text="Journey Through Stories and Experiences"></app-main>
+
+
   
-  
+  <app-conta></app-conta>
   `
 })
 export class ContactComponent {
--- src/components/pages/travel-tips/travel-tips.component.ts
+++ src/components/pages/travel-tips/travel-tips.component.ts
@@ -10,6 +10,17 @@ import { TopComponent } from "../../top/top.component";
 
   template:`
   <app-main image="./trip.jpg"  text="Our Journey: Inspiring Travel, One Trip at a Time"></app-main>
+  <header class="container m-auto d-flex justify-content-between ">
+    <div>
+      <h1 class="metal-mania-regular h1Title">Travel Like a Pro: <br>
+       <span>Tips </span>&<span> Tricks</span></h1>
+    </div>
+    <div class="w-50 fs-4 " >
+      <p style="color: #596365;">Our travel tips page offers essential advice to make your adventures smoother and more enjoyable. From packing hacks to safety tips, you'll find practical insights for every type of traveler. Whether you're exploring cities, hiking mountains, or traveling on a budget, we've got you covered. Discover expert advice, helpful resources, and insider knowledge to ensure your trips are stress-free and unforgettable. Ready to enhance your travel experience? Read more and start planning your next journey!</p>
+    </div>
+  </header>
+
+
   <app-top></app-top>
   `
 
